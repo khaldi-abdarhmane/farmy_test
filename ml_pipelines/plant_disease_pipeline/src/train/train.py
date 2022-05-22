1e5959dc4d57c761c2fc459471d33e4e00ae88b3
hamlahichem <44346868+hamlahichem@users.noreply.github.com>
2022-05-22 12:12:10 +0100

    feat(training pipeline): - add mlflow tracking params and metrics

--- ml_pipelines/plant_disease_pipeline/src/train/train.py
+++ ml_pipelines/plant_disease_pipeline/src/train/train.py
@@ -10,9 +10,18 @@ from params import Params
 from arch_model import Arch_model
 from generator import generator
 import os
+import pandas as pd
+import mlflow
+import mlflow.tensorflow
 
 
-params=params_fct()
+
+
+params =params_fct()
+params_dict= yaml.safe_load(open("./params.yaml"))["train"]
+print("[params]:",params)
+print("[params_dict]:",params_dict)
+
 
 if len(sys.argv) != 5:
     sys.stderr.write("Arguments error. Usage:\n")
@@ -33,25 +42,32 @@ generatorobjet=generator(rescale=params.rescale,
                         )
 
 
-
 base_model_1 = VGG16(include_top =params.include_top,
                      input_shape =params.input_shape)
-
 arch_model=Arch_model(base_model= base_model_1,class_number=generatorobjet.class_number)
-
 model_=arch_model.model
 model_.compile(optimizer=params.optimizer, loss=params.loss, metrics= params.metrics )
 
+# setup mlflow tracking
+mlflow.set_tracking_uri("http://ec2-54-234-36-95.compute-1.amazonaws.com:8080")
+mlflow.set_experiment("ml_pipeline")
 
-history=model_.fit(generatorobjet.train_generator,
+print(output_model)
+with mlflow.start_run() as run:
+    
+    history=model_.fit(generatorobjet.train_generator,
                    epochs= params.nbr_epoch,
                    validation_data=generatorobjet.validation_generator)
+    model_.save(os.path.join(output_model))
 
+    mlflow.log_params(params_dict)
+    mlflow.log_metric("mse",0.99)
+    mlflow.tensorflow.log_model(model_)
 # target_dir = '/home/khaldi-user/Desktop/piplean/data/model'
-model_.save(os.path.join(output_model))
 
-import pandas as pd
 
-history_df = pd.DataFrame(history.history)
-history_df.to_csv(os.path.join(output_history),index=False)
+
+#history_df = pd.DataFrame(history.history)
+#history_df.to_csv(os.path.join(output_history),index=False)
+
 
